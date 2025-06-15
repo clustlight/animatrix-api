d6a3eeb322539dce989a0c9aa7615ebb0c177cc9
quadseed <contact@quadseed.net>
2025-06-15 17:41:15 +0900

    Update SeriesResponse and CreateSeriesRequest to enforce TitleEn and TitleYomi fields

--- internal/types/series.go
+++ internal/types/series.go
@@ -4,14 +4,14 @@ type SeriesResponse struct {
 	SeriesID  string           `json:"series_id"`
 	Title     string           `json:"title"`
 	TitleYomi string           `json:"title_yomi"`
-	TitleEn   string           `json:"title_en,omitempty"`
+	TitleEn   string           `json:"title_en"`
 	Seasons   []SeasonResponse `json:"seasons,omitempty"`
 }
 
 type CreateSeriesRequest struct {
 	SeriesID  string `json:"series_id" validate:"required"`
 	Title     string `json:"title" validate:"required"`
-	TitleYomi string `json:"title_yomi"`
+	TitleYomi string `json:"title_yomi,omitempty"`
 	TitleEn   string `json:"title_en,omitempty"`
 }
 
