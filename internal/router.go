0a5fbe3370ebbccadc45fa809baf9e9d9e39bf25
quadseed <contact@quadseed.net>
2025-06-15 21:09:02 +0900

    Add bulk creation endpoints for series, seasons, and episodes in router

--- internal/router.go
+++ internal/router.go
@@ -15,15 +15,21 @@ func NewRouter(client *ent.Client) *chi.Mux {
 		api.Get("/series/{series_id}", handler.GetSeriesDetail(client))
 		api.Patch("/series/{series_id}", handler.UpdateSeries(client))
 
+		api.Post("/series/bulk", handler.BulkCreateSeriesHandler(client))
+
 		api.Get("/season", handler.GetAllSeasons(client))
 		api.Post("/season", handler.CreateSeason(client))
 		api.Get("/season/{season_id}", handler.GetSeasonDetail(client))
 		api.Patch("/season/{season_id}", handler.UpdateSeason(client))
 
+		api.Post("/season/bulk", handler.BulkCreateSeasonHandler(client))
+
 		api.Get("/episode", handler.GetAllEpisodes(client))
 		api.Post("/episode", handler.CreateEpisode(client))
 		api.Get("/episode/{episode_id}", handler.GetEpisodeDetail(client))
 		api.Patch("/episode/{episode_id}", handler.UpdateEpisode(client))
+
+		api.Post("/episode/bulk", handler.BulkCreateEpisodeHandler(client))
 
 		api.Get("/content/{episode_id}/{filename}", handler.GetContent())
 	})
